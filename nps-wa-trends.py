6e3889541d0428f6452b8218a724596648084980
Ramprakash <ramprax@gmail.com>
2014-07-30 16:07:53 +0530

    Upload a chat txt, download the summary csv

--- nps-wa-trends.py
+++ nps-wa-trends.py
@@ -1,11 +1,45 @@
 
 import os
-from flask import Flask
+from flask import Flask, request, make_response
+from npswa import count_by_date_name, split_time_name
+
+ALLOWED_EXTENSIONS = set(['txt', 'text'])
 
 app = Flask(__name__)
+app.config.from_pyfile('flask_config.py')
 
-@app.route('/')
+@app.route('/hello')
 def hello():
     return 'Hello World!'
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        upfile = request.files['upfile']
+        upfilename = upfile.filename
+        if upfile and allowed_file(upfile.filename):
+            return generate_summary_resp(upfile, upfilename)
+    return '''
+    <!doctype html>
+    <title>NPS chat summary</title>
+    <h1>Upload chat txt file</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=upfile>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+def generate_summary_resp(upfile, upfilename):
+    headers = {"Content-Disposition": "attachment; filename=%s" % ('summary.csv')}
+    csv_content = []
+    for line in count_by_date_name(split_time_name(upfile.stream)):
+        csv_content.append(line)
+    csv_content = '\n'.join(csv_content)
+    print type(csv_content), len(csv_content)
+    return make_response((csv_content, None, headers))
+        
+if __name__=='__main__':
+    print "ha"
--- npswa.py
+++ npswa.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 
+import re
 from collections import OrderedDict
 
-def split_time_name(filename):
+def split_time_name(openfilestream):
     date_fmt = 'dd/MM/yyyy'
     datetime_fmt = date_fmt + ' hh:mm:ss aa'
-    with open(filename) as fd:
-        for line in fd:
-            line = line.strip()
-            if line:
-                if len(line) <= len(datetime_fmt) or ':' != line[len(datetime_fmt)]:
-                    continue                
-                datetime_str = line[:len(datetime_fmt)].strip()
-                datestr = datetime_str[:len('dd/MM/yyyy')].strip()
-                rest = line[len(datetime_fmt)+1:].strip()
-                name_end = rest.find(':')
-                if name_end >= 0:
-                    name = rest[:name_end].strip()
-                    msg = rest[name_end+1:].strip()
-                    yield datestr, datetime_str, name, msg
+    datematchprog = re.compile('/2014 ([1-9]):')
+    replacement = '/2014 0\\1:'
+    for line in openfilestream:
+        line = line.strip()
+        if line:
+            line = datematchprog.sub(replacement , line)
+            #print line
+            if len(line) <= len(datetime_fmt) or ':' != line[len(datetime_fmt)]:
+                continue                
+            datetime_str = line[:len(datetime_fmt)].strip()
+            datestr = datetime_str[:len('dd/MM/yyyy')].strip()
+            rest = line[len(datetime_fmt)+1:].strip()
+            name_end = rest.find(':')
+            if name_end >= 0:
+                name = rest[:name_end].strip()
+                msg = rest[name_end+1:].strip()
+                yield datestr, datetime_str, name, msg
 
 def count_by_date_name(iterable):
     date_name_count = OrderedDict()
@@ -39,7 +43,7 @@ def count_by_date_name(iterable):
             date_name_count[datestr] = name_count
             name_count[name] = 1
     name_list.sort()
-    print ','.join(('Date', ','.join(name_list)))
+    yield ','.join(('Date', ','.join(name_list)))
     totcount = 0
     for datestr in date_name_count:
         name_count = date_name_count[datestr]
@@ -51,13 +55,14 @@ def count_by_date_name(iterable):
                 totcount = totcount + name_count[name]
             else:
                 rowarr.append('0')
-        print ','.join(rowarr)
+        yield ','.join(rowarr)
     #print totcount
 
 if __name__ == '__main__':
     #for l in split_time_name('nps-wa-29-jul-2014_0928.txt'):
     #    print l
-    count_by_date_name(split_time_name('nps-wa-29-jul-2014_0928.txt'))
-
-
-
+    import sys
+    filename = sys.argv[1]
+    with open(filename) as fd:
+        for line in count_by_date_name(split_time_name(fd)):
+            print line
